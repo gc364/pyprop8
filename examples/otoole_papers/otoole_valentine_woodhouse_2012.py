05a139974e0628151ba2823f8c7a9446cd1f21e1
Oscar Humphreys <oscarh24105@hotmail.com>
2026-05-07 15:15:31 +0100

    Adds kernel comparison to example

--- examples/otoole_papers/otoole_valentine_woodhouse_2012.py
+++ examples/otoole_papers/otoole_valentine_woodhouse_2012.py
@@ -5,6 +5,7 @@ from pyprop8.utils import rtf2xyz,make_moment_tensor,stf_trapezoidal,stf_cosine,
 import numpy as np
 import matplotlib.pyplot as plt
 import torch as tp
+from tqdm import tqdm
 
 '''This example aims to reproduce the first couple of figures presented in
 O'Toole, Valentine & Woodhouse (2012, doi: 10.1111/j.1365-246X.2012.05608.x).
@@ -27,9 +28,12 @@ print(model)
 stations = pp.RegularlyDistributedReceivers(39.6,39.6,5,90-118.2,90-118.2,5,degrees=True).asListOfReceivers()
 
 # Table 2, 'Iteration 0' column
-event = pp.PointSource(tp.tensor(0),tp.tensor(0),tp.tensor(np.deg2rad(35)),rtf2xyz(tp.tensor([[ 0.3406, 0.0005, 0.1610],
-                                                                                [ 0.0005, 0.7798, 0.1430],
-                                                                                [ 0.1610, 0.1430, 0.6349]])),tp.tensor([[0.],[0.],[0.]]),0)
+
+Mrtf = tp.tensor([[ 0.3406, 0.0005, 0.1610],
+                [ 0.0005, 0.7798, 0.1430],
+                [ 0.1610, 0.1430, 0.6349]]).requires_grad_()
+
+event = pp.PointSource(tp.tensor(0),tp.tensor(0),tp.tensor(35),rtf2xyz(Mrtf),tp.tensor([[0.],[0.],[0.]]),0)
 drv = pp.DerivativeSwitches(moment_tensor=True,z=True,x=True,y=True,time=True)
 
 # Appears that the paper just uses the cosine low-pass filter
@@ -39,6 +43,30 @@ stf = lambda w: clp_filter(w,0.05*2*np.pi,0.2*2*np.pi)
 tt,seis,deriv = pp.compute_seismograms(model,event,stations,81,0.5,source_time_function = stf,derivatives=drv,pad_frac=0.5)
 seis = seis[0]
 deriv = deriv[0]
+
+pt_derivs = tp.zeros_like(deriv) #(6,3,nt)
+deriv = deriv.detach().numpy()
+for i in range(seis.shape[0]):
+    for j in tqdm(range(seis.shape[1])):
+        g0 = tp.zeros_like(seis)
+        g0[i,j] = 1
+        seis.backward(g0,retain_graph=True)
+        grad = Mrtf.grad
+        #   Just diagonal for now
+        for k in range(3):
+            pt_derivs[k,i,j] = grad[k,k]
+        inds = [[0,1],[0,2],[1,2]]
+      
+        k=3
+        for ind in inds:
+            pt_derivs[k,i,j] = grad[ind[0],ind[1]]
+            pt_derivs[k,i,j] += grad[ind[1],ind[0]]
+            k+=1
+
+        Mrtf.grad = tp.zeros_like(Mrtf)
+
+pt_derivs = pt_derivs.detach().numpy()
+seis = seis.detach().numpy()
 nez = [1,0,2] #Reorder seismogram components to match O'Toole's figure
 # Native ordering of moment tensor components is as follows:
 #    Mxx, Myy, Mzz, Mxy, Mxz, Myz
@@ -71,6 +99,7 @@ for idrv in range(6):
                 ax.set_title("Vertical")
         ax.plot(tt,np.zeros_like(tt),'k:')
         ax.plot(tt,dcompsign[idrv]*deriv[drv.i_mt+dcomp[idrv],nez[icomp],:])
+        ax.plot(tt,pt_derivs[drv.i_mt+idrv,nez[icomp],:],'r--')
         ax.set_ylim(-amax,amax)
         ax.set_xticks([])
         ax.set_yticks([])
@@ -84,6 +113,9 @@ deriv[drv.i_z,:,:]*=10
 deriv[drv.i_x,:,:]*=10
 deriv[drv.i_y,:,:]*=10
 deriv[drv.i_time,:,:]*=2
+
+
+
 amax = abs(deriv[drv.i_mt+6:,:,:]).max()
 amax = 1.025*max([amax,abs(seis).max()])
 dcomp = [drv.i_z,drv.i_y,drv.i_x,drv.i_time]
